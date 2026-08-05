67ddcda1ce45f976e664d9d81aea948d3caccb63
Emanuele Greco <emanuele1.greco@mail.polimi.it>
2026-08-05 23:37:21 +0200

    Minor changes in crc and climate

--- custom_components/ksenia_lares/climate.py
+++ custom_components/ksenia_lares/climate.py
@@ -222,7 +222,7 @@ class KseniaClimateEntity(KseniaEntity, ClimateEntity):
             return None
         try:
             return float(raw)
-        except ValueError, TypeError:
+        except (ValueError, TypeError):
             return None
 
     @property
@@ -234,7 +234,7 @@ class KseniaClimateEntity(KseniaEntity, ClimateEntity):
             return None
         try:
             return float(val)
-        except ValueError, TypeError:
+        except (ValueError, TypeError):
             return None
 
     @property
